452c1cb030923303657652eda06b1e1067c14f9e
‘Alexander <alexkim.347@gmail.com>
2019-08-18 01:32:51 -0700

    search results single-page with mock data

---
+++ web/src/components/ListingCard/ListingCard.tsx
@@ -0,0 +1,69 @@
+import React from 'react'
+import './ListingCard.scss'
+import ContentLoader from 'react-content-loader'
+import { Link } from 'react-router-dom';
+
+const MyLoader = () => (
+  <ContentLoader height={380} width={280} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
+    <rect x='0' y='0' rx='4' ry='4' width='338' height='255' />
+    <rect x='8' y='275' rx='4' ry='4' width='264' height='15' />
+    <rect x='8' y='305' rx='4' ry='4' width='264' height='15' />
+    <rect x='8' y='340' rx='4' ry='4' width='264' height='15' />
+  </ContentLoader>
+)
+const userIcon: string = './img/user-icon.svg'
+
+export interface IListingInfo {
+  imgURL: string
+  title: string
+  subtitle: string
+  beds: number
+  bath: number
+  cost: number
+  sqft: number
+  src: string
+}
+
+interface IListingCardProps {
+  loading: boolean
+  info: IListingInfo
+}
+
+class ListingCard extends React.Component<IListingCardProps, {}> {
+  showIfLoaded = () => {
+    if (this.props.loading) {
+      return (
+        <div>
+          <MyLoader />
+        </div>
+      )
+    } else {
+      return (
+        <Link to={`/`}>
+          <div className='photoSection'>
+            <img className='peekPhoto' src={this.props.info.imgURL} alt='rental-unit-photo' />
+            {/* <img className='userIcon' src={userIcon} alt='user-icon'/> */}
+          </div>
+          <div className='info'>
+
+            <div className='upper-info'>
+                <h1>{this.props.info.title}</h1>
+                <h3>{this.props.info.subtitle}</h3>
+                <br/>
+                <h3>{this.props.info.beds} Beds - {this.props.info.bath} Baths</h3>
+            </div>
+            <div className='lower-info'>
+                <span><h1 className='inline'><strong>{this.props.info.cost}</strong></h1><h4 className='inline'> / Month</h4></span>
+                <span><h4>{this.props.info.sqft} SQFT</h4></span>
+            </div>
+          </div>
+        </Link>
+      )
+    }
+  }
+
+  render() {
+    return <div className='listingCard'>{this.showIfLoaded()}</div>
+  }
+}
+export default ListingCard
--- web/src/containers/Listings/Listings.tsx
+++ web/src/containers/Listings/Listings.tsx
@@ -1,24 +1,172 @@
 import React from 'react'
+import './Listings.scss'
+import ListingCard, { IListingInfo } from '../../components/ListingCard/ListingCard'
 
-class Listings extends React.Component<{},{}> {
-render() {
-    return (
-        <div>
-            <p>results</p>
-        </div>
-    )
+const marketingMumbo: string = '/img/Marketing-mumbo.svg'
+const livImage: string = '/img/Marketing-mumbo-use-liv.svg'
+const altImage: string = '/img/Marketing-mumbo-use-everything-else.svg'
+const emptyListing: IListingInfo = {
+  imgURL: '',
+  title: '',
+  subtitle: '',
+  beds: 0,
+  bath: 0,
+  cost: 0,
+  sqft: 0,
+  src: ''
 }
-} export default Listings
 
+interface IListingsState {
+  loading: boolean
+  allListings: IListingInfo[]
+  maxResults: number
+}
 
-const mockData = [
-    {
-        title: 'title here',
-        subtitle: 'subtitle here',
-        beds: '6',
-        bath: '3',
-        cost: '5000',
-        sqft: '345',
-        src: 'www.facebook.com/marketplace'
+class Listings extends React.Component<{}, IListingsState> {
+  state = {
+    loading: true, // important in-case user wants to reload or change search criteria
+    allListings: [],
+    maxResults: 30
+  }
+
+  componentWillMount = () => {
+    // show the placeholder items
+    // check if still loading
+    this.setState({
+      loading: false,
+      allListings: mockData
+    })
+  }
+
+  renderAllListings = () => {
+    if (this.state.loading === true) {
+      for (let i = 0; i < this.state.maxResults; i++) {
+        this.displaySingleListing(emptyListing, i)
+      }
+    } else {
+      return this.state.allListings.map(this.displaySingleListing)
     }
+  }
+
+  displaySingleListing = (info: IListingInfo, index: number) => {
+    console.log(index, info)
+    return <ListingCard key={index} loading={this.state.loading} info={info} />
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='searchBar'></div>
+        <div className='banner'>
+          <div className='banner-overlay'>
+            <div className='banner-text'>
+              <h1 className='banner-title white-text'>Grow organic traffic</h1>
+              <h3 className='white-text'>By creating a one-stop site for all prospective renters</h3>
+              <div className='illustration-overlay'>
+                <img className='inline-img' src={altImage} alt='use-everything' />
+                <img className='inline-img' src={livImage} alt='use-liv' />
+              </div>
+            </div>
+          </div>
+        </div>
+            <div className='searchResults'>
+        {this.renderAllListings()}
+            </div>
+      </div>
+    )
+  }
+}
+export default Listings
+
+const mockData: IListingInfo[] = [
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 1',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 2',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 3',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 4',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 5',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 6',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 7',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 8',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  },
+  {
+    imgURL: 'https://d2jydbnljbirzw.cloudfront.net/fit-in/735x500/filters:quality(100)/static_files/unit/2396/unit_2396_5d5886b65ad61_blob',
+    title: 'title here 9',
+    subtitle: 'subtitle here',
+    beds: 6,
+    bath: 3,
+    cost: 5000,
+    sqft: 345,
+    src: 'https://www.facebook.com/marketplace'
+  }
 ]
