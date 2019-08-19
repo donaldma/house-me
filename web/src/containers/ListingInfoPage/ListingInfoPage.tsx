ec9e3829e70a660fa5c4e721bb18da19127e73a8
oklex <alexkim.347@gmail.com>
2019-08-19 02:55:00 -0700

    page loading

--- web/src/containers/ListingInfoPage/ListingInfoPage.tsx
+++ web/src/containers/ListingInfoPage/ListingInfoPage.tsx
@@ -3,6 +3,21 @@ import { RouteComponentProps, withRouter, Link } from 'react-router-dom'
 import './ListingInfoPage.scss'
 import Modal from 'react-bootstrap/Modal'
 import Form from 'react-bootstrap/Form'
+import ContentLoader from 'react-content-loader'
+
+const CarouselLoader = () => (
+  <ContentLoader height={350} width={1050} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
+    <rect x='12' y='21' rx='0' ry='0' width='1025' height='350' />
+  </ContentLoader>
+)
+
+const MyPageLoader = () => (
+  <ContentLoader height={500} width={1110} speed={2} primaryColor='#f3f3f3' secondaryColor='#ecebeb'>
+    <rect x='750' y='50' rx='3' ry='3' width='350' height='200' />
+    <rect x='12' y='50' rx='3' ry='3' width='700' height='25' />
+    <rect x='12' y='100' rx='3' ry='3' width='700' height='150' />
+  </ContentLoader>
+)
 
 interface IListingInfoPageProps extends RouteComponentProps {
   // webListingID:number
@@ -22,25 +37,24 @@ class ListingInfoPage extends React.Component<IListingInfoPageProps, IListingInf
     messageSent: false
   }
 
+  componentWillMount = () => {
+    // call api
+    // update loading state when done
+    this.setState({
+      loading: false
+    })
+  }
+
   openModal = () => {
     this.setState({
-      modalShow: true,
+      modalShow: true
     })
   }
 
   closeModal = () => {
     this.setState({
-        modalShow: false,
+      modalShow: false,
       messageSent: false
-      })
-  }
-
-  
-
-  componentWillMount = () => {
-    // call api here
-    this.setState({
-      loading: false
     })
   }
 
@@ -52,11 +66,13 @@ class ListingInfoPage extends React.Component<IListingInfoPageProps, IListingInf
             <Modal.Title>Message sent!</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-                <p>message sent!</p>
+            <p>message sent!</p>
           </Modal.Body>
           <Modal.Footer>
-            <button onClick={()=>this.setState({messageSent:false})}>Go back</button>
-            <Link to='/about'><button>About us</button></Link>
+            <button onClick={() => this.setState({ messageSent: false })}>Go back</button>
+            <Link to='/about'>
+              <button>About us</button>
+            </Link>
           </Modal.Footer>
         </div>
       )
@@ -67,7 +83,7 @@ class ListingInfoPage extends React.Component<IListingInfoPageProps, IListingInf
             <Modal.Title>Write a message</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-          <div>
+            <div>
               <Form>
                 <Form.Group controlId='formBasicSubject'>
                   <Form.Label>Email Topic</Form.Label>
@@ -77,7 +93,7 @@ class ListingInfoPage extends React.Component<IListingInfoPageProps, IListingInf
 
                 <Form.Group controlId='formBasicMessage'>
                   <Form.Label>Message</Form.Label>
-                  <Form.Control as="textarea" rows="10"/>
+                  <Form.Control as='textarea' rows='10' />
                 </Form.Group>
                 <Form.Group controlId='formBasicChecbox'>
                   <Form.Check type='checkbox' label='some kind of captcha' />
@@ -86,28 +102,52 @@ class ListingInfoPage extends React.Component<IListingInfoPageProps, IListingInf
             </div>
           </Modal.Body>
           <Modal.Footer>
-            <button onClick={()=>this.setState({messageSent:true})}>Send message; go to next modal content panel</button>
+            <button onClick={() => this.setState({ messageSent: true })}>
+              Send message; go to next modal content panel
+            </button>
           </Modal.Footer>
         </div>
       )
     }
   }
 
+  showContactCard = () => {
+    return <div className='col-md-3'>
+      <div className='side-bar'>
+        Rent / month
+          <button onClick={() => this.openModal()}>click to show modal</button>
+          <Modal show={this.state.modalShow} onHide={this.closeModal}>
+            {this.showModalMessage()}
+          </Modal>
+      </div>
+    </div>
+  }
+
   showIfLoaded = () => {
     if (this.state.loading) {
       return (
         <div>
-          <p>Loading...</p>
+          <CarouselLoader />
+          <div className='container'>
+            <MyPageLoader />
+          </div>
         </div>
       )
     } else {
       return (
         <div>
-          <p>details</p>
-          <button onClick={() => this.openModal()}>click to show modal</button>
-          <Modal show={this.state.modalShow} onHide={this.closeModal}>
-            {this.showModalMessage()}
-          </Modal>
+          <div>
+            
+          <h1>photos</h1>
+          </div>
+          <div className='container'>
+            <div className='row'>
+              <div className='col-md-9'>
+                <p> content</p>
+              </div>
+              {this.showContactCard()}
+            </div>
+          </div>
         </div>
       )
     }
